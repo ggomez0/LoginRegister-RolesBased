dd4c3084a7578609e3526b11960d5193d08c273c
Gaspar <gaspargomez2000@outlook.com>
2023-07-15 12:55:32 -0300

    Rolebased

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,52 @@
 import './App.css'
 import { MyRoutes } from './routes/routes'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {firebase} from "./firebase"
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+const auth = getAuth(firebase);
+const firestore = getFirestore(firebase);
 
 function App() {
-  const nombreUsuario = "";
-  return (
-       <div>
-        <MyRoutes/>
-       </div>
-  )
+  const [user, setUser] = useState(null);
+
+  async function getRol(uid) {
+    const docuRef = doc(firestore, `usuarios/${uid}`);
+    const docuCifrada = await getDoc(docuRef);
+    const infoFinal = docuCifrada.data().rol;
+    return infoFinal;
+  }
+
+  function setUserWithFirebaseAndRol(usuarioFirebase) {
+    getRol(usuarioFirebase.uid).then((rol) => {
+      const userData = {
+        uid: usuarioFirebase.uid,
+        email: usuarioFirebase.email,
+        rol: rol,
+      };
+      setUser(userData);
+      console.log("userData final", userData);
+    });
+  }
+
+  onAuthStateChanged(auth, (usuarioFirebase) => {
+    if (usuarioFirebase) {
+      //funcion final
+
+      if (!user) {
+        setUserWithFirebaseAndRol(usuarioFirebase);
+      }
+    } else {
+      setUser(null);
+    }
+  });
+
+  return <>{user ? <Home user={user} /> : <Login />}</>;
 }
+  // return (
+  //      <div>
+  //       <MyRoutes/>
+  //      </div>
+  // )
+
 
 export default App
---
+++ src/components/Admin/Admin.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Admin() {
+  return <div>Hola, admin</div>;
+}
+
+export default Admin;
