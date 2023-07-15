281f5a36e527a5510468a4efa4662675e28804dd
Gaspar <gaspargomez2000@outlook.com>
2023-07-14 22:33:30 -0300

    login-reg

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import { Login } from "../components/Login/Login";
-import { Home } from "../components/home/home";
+import { Home } from "../components/Home/Home";
 import { Signup } from "../components/Signup/Signup";
 import { useEffect, useState } from "react";
 import { auth } from "../firebase";
